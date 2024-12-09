0c18b720d28be9e76be1f8ac605c199d59e226ea
Кірюша <mkiril087@gmail.com>
2024-12-09 23:14:17 +0200

    пр

---
+++ index1.js
@@ -0,0 +1,42 @@
+// task1
+
+// let massyv = [`qw`, `uw`, `qa`, `tr`, `op`, 1, 23, 90];
+// // console.log();
+
+
+
+// function logItem (x) {
+//     for (let i = 0; i < x.length; i += 1) {
+//         let b = x[i];
+//         console.log(`elementNum ${x.indexOf(b) + 1}, ${x[i]}`);
+        
+//     }
+//      return
+// }
+
+// logItem(massyv);
+
+// task3
+
+let wertw = prompt(`wdjkfvndknfvkjnrb`);
+let message = wertw.split(` `);
+// let massuv = [message]
+// console.log(message[1]);
+let xcvb = [];
+
+for (let i = 0; i < message.length; i += 1) {
+    // console.log(message[i].length);
+    xcvb.push(message[i].length)
+    
+    // return
+}
+// console.log(x);
+
+// console.log(xcvb.max(xcvb[0], xcvb[1]));
+
+
+console.log(Math.max(xcvb[0], xcvb[1]));
+
+function findLongestWord(string) {
+}
+findLongestWord(message);
